17ffb1dd1076c337ef25a63cd992b65afdd48201
Shiyu Wang <shiyu3169@gmail.com>
2018-07-24 09:41:50 -0400

    navbar and button

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main'
+import Navbar from './components/Navbar'
+import {Link} from 'react-router-dom'
+
 const App = () => (
   <div>
-    <Main />
+  	<Navbar />
+  	<div className="container">
+    	<Main />
+    </div>
+    <div className="fixed-action-btn">
+    	<Link to="/meetups/add" className="btn-floating btn-large red">
+    		<i className="fa fa-plus"></i>
+    	</Link>
+    </div>
   </div>
 )
 
---
+++ client_src/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
+
+class Navbar extends Component {
+	render(){
+		return (
+			<div>
+				<nav className="blue darken-3">
+				    <div className="nav-wrapper">
+				      	<a href="/" className="brand-logo">Meetupz</a>
+				      	<a data-target="main-menu" className="sidenav-trigger show-on-large">
+				      		<i className="fa fa-bars"></i>
+				     	</a>
+				      	<ul className="right hide-on-small-only">
+				        	<li>
+				        		<Link to="/"><i className="fa fa-users"></i> Meetups</Link>
+				        	</li>
+				      	</ul>
+				    </div>
+				</nav>
+				<ul className="sidenav" id="main-menu">
+					<li>
+		        		<Link to="/"><i className="fa fa-users"></i> Meetups</Link>
+		        	</li>
+		        	<li>
+		        		<Link to="/meetups/add"><i className="fa fa-plus"></i>Add Meetup</Link>
+		        	</li>
+			  	</ul>
+			</div>
+		)
+	}
+}
+
+export default Navbar;
